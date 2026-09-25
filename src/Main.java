618df871074f21e7e9e8db81d7c8f3b681c49a34
Mickayla Beckett <beckettmickayla@gmail.com>
2026-09-25 08:45:43 -0400

    ShipCostCalculator

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 void main() {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter your dinner choice of C, F, or V: ");
-    String dinnerChoice;
-    dinnerChoice = scanner.next();
-    if (dinnerChoice.equalsIgnoreCase("C")) {
-        System.out.println("You get the Chicken Parmesan.");
-    } else if (dinnerChoice.equalsIgnoreCase("F")) {
-        System.out.println("You get the Roast Salmon.");
-    } else if (dinnerChoice.equalsIgnoreCase("V")) {
-        System.out.println("You get the Butternut Squash.");
+    Scanner in = new Scanner(System.in);
+    double itemPrice = 0;
+    String Trash = "";
+    double totalCost = 0;
+    System.out.print("Enter your item price: ");
+    if (in.hasNextDouble()) {
+        itemPrice = in.nextDouble();
+        in.nextLine(); // Remember to clear the buffer here
+        if (itemPrice>=100) {
+            System.out.print("Your shipping is free and your total cost is " + itemPrice);
+        } else if (itemPrice<100) {
+            totalCost = (itemPrice * 0.02) + itemPrice;
+            System.out.print("Your total cost with shipping is " + totalCost);
+        }
     } else {
-        System.out.println("Invalid choice. Please select C, F, or V.");
+        Trash = in.nextLine();
+        System.out.print("Enter a valid item price. You entered: " + Trash);
     }
-
-    scanner.close();
 }
-
