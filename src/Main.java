7bd30a57478be5dbb18f47f19ea9a39249adb395
Mickayla Beckett <beckettmickayla@gmail.com>
2026-09-25 08:43:29 -0400

    RSVPMenu

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
 void main() {
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter your age in years: ");
-    int age = scanner.nextInt();
-    if (age >= 21) {
-        System.out.print("You are 21 or older. You get a wristband.");
+    System.out.print("Enter your dinner choice of C, F, or V: ");
+    String dinnerChoice;
+    dinnerChoice = scanner.next();
+    if (dinnerChoice.equalsIgnoreCase("C")) {
+        System.out.println("You get the Chicken Parmesan.");
+    } else if (dinnerChoice.equalsIgnoreCase("F")) {
+        System.out.println("You get the Roast Salmon.");
+    } else if (dinnerChoice.equalsIgnoreCase("V")) {
+        System.out.println("You get the Butternut Squash.");
     } else {
-         System.exit(0);
+        System.out.println("Invalid choice. Please select C, F, or V.");
     }
+
+    scanner.close();
 }
 
