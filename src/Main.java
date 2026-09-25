7712c227e8531ddc41f018654ac6aff2166069ff
Mickayla Beckett <beckettmickayla@gmail.com>
2026-09-25 08:04:01 -0400

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    Scanner in = new Scanner(System.in);
+    double itemPrice = 0;
+    String Trash = "";
+    double totalCost = 0;
+    System.out.print("Enter your item price: ");
+    if (in.hasNextDouble()) {
+        itemPrice = in.nextDouble();
+        in.nextLine(); // Remember to clear the buffer here
+        if (itemPrice>=100) {
+            System.out.print("Your shipping is free and your total cost is " + itemPrice);
+        } else if (itemPrice<100) {
+            totalCost = (itemPrice * 0.02) + itemPrice;
+            System.out.print("Your total cost with shipping is " + totalCost);
+        }
+    } else {
+        Trash = in.nextLine();
+        System.out.print("Enter a valid item price. You entered: " + Trash);
     }
 }
+
